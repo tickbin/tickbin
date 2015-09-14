2056c7e1b6e9717f8b24fb2158228991af5937c6
Jonathan Bowers <jonotron@gmail.com>
2015-09-13 18:09:19 -0700

    tick-log accepts message or prompts for message

--- commands/tick-log.js
+++ commands/tick-log.js
@@ -1,14 +1,36 @@
 export default log
 
+import Entry from '../entry'
+import prompt from 'prompt'
+
 function log (yargs) {
   let argv = yargs
   .usage('tick log')
   .option('date', {
     alias: 'd',
-    describe: 'date for tick',
-    demand: true
+    describe: 'date for tick'
+  })
+  .option('message', {
+    alias: 'm',
+    describe: 'entry message. E.g. 8am-12pm fixing bugs #tickbin'
   })
   .help('h')
   .alias('h', 'help')
   .argv
+
+  const {message, date} = argv
+  if (!message) {
+    prompt.start()
+    prompt.get('message', function(err, res) {
+      if (!err) createEntry(res.message, {date}) 
+    })
+  } else {
+    createEntry(message, {date}) 
+  }
+}
+
+function createEntry (message, opts = {}) {
+  let entry = new Entry(message, opts)
+
+  console.log(entry.toJSON())
 }
