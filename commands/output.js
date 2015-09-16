b9517da45c8bc4234b73f84636733605404fca2f
Jonathan Bowers <jonotron@gmail.com>
2015-09-15 19:51:50 -0700

    format date as Sun Sep 15 with moment

--- commands/output.js
+++ commands/output.js
@@ -1,5 +1,6 @@
 export {write as write}
+import moment from 'moment'
 
 function write(entry) {
-  console.log(`${entry._id} ${entry.startDate} ${entry.message}`)
+  console.log(`${entry._id} ${moment(entry.startDate).format('ddd MMM DD')} ${entry.message}`)
 }
