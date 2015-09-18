4e43c196398ee5c5eb3fdcdfa55bccad96d0d458
Jonathan Bowers <jonotron@gmail.com>
2015-09-18 15:19:05 -0700

    list out entries from the db

--- commands/tick-list.js
+++ commands/tick-list.js
@@ -1,9 +1,34 @@
 export default list
 
+import Entry from '../entry'
+import PouchDB from 'pouchdb'
+import {write} from './output'
+
+let db = new PouchDB('tickbin')
+
 function list (yargs) {
   let argv = yargs
   .usage('tick list')
   .help('h')
   .alias('h', 'help')
   .argv
+
+  db.query(mapDate, {
+    include_docs: true,
+    descending: true
+  }).then(writeEntries)
+
+}
+
+function mapDate (doc) {
+  emit(doc._id)
+}
+
+function writeEntries (results) {
+  results.rows.forEach(function(row) {
+    // this is likely slow as it requires creating a new entry
+    // but write requires an instatiated Entry at the moment
+    write(new Entry(row.doc.message))     
+  })
+
 }
