3d91dbffef0cf6ca4fca6e4165e9a0f78c6eb567
Jonathan Bowers <jonotron@gmail.com>
2015-09-23 19:16:38 -0700

    list groups by date

--- commands/tick-list.js
+++ commands/tick-list.js
@@ -3,6 +3,7 @@ export default list
 import Entry from '../entry'
 import PouchDB from 'pouchdb'
 import {write} from './output'
+import moment from 'moment'
 
 let db = new PouchDB('tickbin')
 
@@ -25,7 +26,12 @@ function mapDate (doc) {
 }
 
 function writeEntries (results) {
+  let prevDate = null
   results.rows.forEach(function(row) {
+    const e = Entry.fromJSON(row.doc)
+    if (!moment(prevDate).isSame(e.from, 'day'))
+      console.log(moment(e.from).format('ddd, MMM DD, YYYY'))
+    prevDate = e.from
     // this is likely slow as it requires creating a new entry
     // but write requires an instatiated Entry at the moment
     write(Entry.fromJSON(row.doc))     
