5499034c94d933502d5dabb9c3d267fccf3a0a77
Jonathan Bowers <jonotron@gmail.com>
2015-09-28 08:35:09 -0700

    show time duration in each entry

--- commands/output.js
+++ commands/output.js
@@ -1,7 +1,12 @@
 export {write as write}
 import moment from 'moment'
 import chalk from 'chalk'
+import format from '../time'
 
 function write(entry) {
-  console.log(`${chalk.gray(entry._id)} ${chalk.yellow(moment(entry.from).format('ddd MMM DD'))} ${entry.message}`)
+  const id = `${chalk.gray(entry._id)}`
+  const date = `${chalk.yellow(moment(entry.from).format('ddd MMM DD'))}` 
+  const time = chalk.green(format(entry.duration.minutes))
+  const msg = `${entry.message}` 
+  console.log(`${id} ${date} ${time} ${msg}`)
 }
