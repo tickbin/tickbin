4ebceffb470ef97281ce84f35a999dd1790b7eec
Jonathan Bowers <jonotron@gmail.com>
2015-09-29 09:53:08 -0700

    pad the outputs

--- commands/output.js
+++ commands/output.js
@@ -1,6 +1,7 @@
 import moment from 'moment'
 import chalk from 'chalk'
 import format from '../time'
+import pad from 'pad'
 
 export {write as write}
 export {getOutputs as getOutputs}
@@ -12,8 +13,8 @@ function write(entry) {
 }
 
 function getOutputs(entry) {
-  const id = `${chalk.gray(entry._id)}`
-  const date = `${chalk.yellow(moment(entry.from).format('ddd MMM DD'))}` 
+  const id = `${chalk.gray(pad(entry._id, 10))}`
+  const date = `${chalk.yellow(pad(moment(entry.from).format('ddd MMM DD'),9))}` 
   const time = chalk.green(format(entry.duration.minutes))
   const msg = `${entry.message}` 
   const detailed = `${id} ${date} ${time} ${msg}`
--- time.js
+++ time.js
@@ -1,8 +1,9 @@
 export default format
+import pad from 'pad'
 
 function format (minutes) {
   const hours = Math.floor(minutes / 60)
   const mins = minutes % 60
 
-  return `${hours}h ${mins}m`
+  return `${pad(2, hours)}h ${pad(2,mins)}m`
 }
