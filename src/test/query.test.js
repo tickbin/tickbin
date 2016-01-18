7828e12139f5e3f91d67132a91a75a9088cbbc83
Jonathan Bowers <jonotron@gmail.com>
2016-01-18 12:08:09 -0800

    filterTags accepts a row, not a tag array

--- src/test/query.test.js
+++ src/test/query.test.js
@@ -2,11 +2,12 @@ import test from 'tape'
 
 import { filterTags } from '../query'
 
-test('filterTags() finds tags in source using OR', t => {
-  const src = ['a', 'b', 'c', 'd']
-  t.ok(filterTags(['a'], src), 'finds single tag')
-  t.ok(filterTags(['a', 'b'], src), 'finds multiple tags')
-  t.ok(filterTags(['a', 'x'], src), 'finds any matching tag')
+test('filterTags() finds tags in source using AND', t => {
+  const row = { doc: { tags: ['a', 'b', 'c', 'd'] }}
+  t.ok(filterTags(['a'], row), 'finds single tag')
+  t.ok(filterTags(['a', 'b'], row), 'finds multiple tags')
+  t.notOk(filterTags(['x'], row), 'does not find single tag')
+  t.notOk(filterTags(['a', 'x'], row), 'does not find multiple tags')
 
   t.end()
 })
