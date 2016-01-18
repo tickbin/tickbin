28122876665e923806eaa6af7c078211d6142e56
Jonathan Bowers <jonotron@gmail.com>
2016-01-18 12:08:41 -0800

    filterTags() performs AND filter

--- src/query.js
+++ src/query.js
@@ -1,5 +1,14 @@
 export { filterTags }
 
-function filterTags () {
+import every from 'lodash/collection/every'
+import includes from 'lodash/collection/includes'
 
+function filterTags (tags = [], row) {
+  if (!tags) // no tags provided, filter nothing
+    return true
+
+  const docTags = row.doc.tags || [] 
+
+  const found = every(tags, t => includes(docTags, t))
+  return found
 }
