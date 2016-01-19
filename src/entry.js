403856084147ac4d9b891629004310f73ceb0f7d
Chad Fawcett <me@chadf.ca>
2016-01-19 10:48:35 -0800

    Clean up json duration declaration

--- src/entry.js
+++ src/entry.js
@@ -69,11 +69,11 @@ export default class Entry {
       toArr: this.toArr,
       time: this.time,
       tags: [...this.tags],
-      duration: {
-        seconds: this.duration ? this.duration.seconds : null,
-        from: this.duration ? this.duration.from : null,
-        to: this.duration ? this.duration.to : null
-      }
+      duration: this.duration ? {
+        seconds: this.duration.seconds,
+        from: this.duration.from,
+        to: this.duration.to
+      } : null
     } 
   }
 
