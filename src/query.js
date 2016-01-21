2f7c3768d9fcb988c15af74813cf81c0184c2306
Jonathan Bowers <jonotron@gmail.com>
2016-01-21 10:23:09 -0800

    add documentation for each function

--- src/query.js
+++ src/query.js
@@ -7,7 +7,15 @@ export { filterTags }
 export { hashTags }
 export { parseDateRange }
 
+/**
+ * Prepare queries on Entries and execute them
+ */
 export default class Query {
+
+  /**
+   * create a new query with some basic setup
+   * requires a db instance (which is a pouchDB/couchDB compatible API)
+   */
   constructor (db) {
     if (!db) throw new Error('Please provide a couchdb instance')
 
@@ -20,6 +28,9 @@ export default class Query {
       .pluck('doc') // each rows has a doc 
   }
 
+  /**
+   * prepare query to find entries by date range filtered by tags
+   */
   findEntries ({start = null, end = null, tags = []} = {}) {
     this._queryOpts.descending = true
     this._queryOpts.startkey = end
@@ -30,6 +41,15 @@ export default class Query {
     return this 
   }
 
+  /**
+   * adds grouping by date to the filter chain
+   * causes the result set to be structured as an array of
+   * {
+   *   entries: [ ], // an array of Entry objects falling within the date
+   *   date: aSingleDay, // a single day grouping multiple entries
+   *   minutes: sum // a reduction of the minutes of that day's entries
+   * }
+   */
   groupByDate () {
     this._chain = this._chain
       .map(doc => Entry.fromJSON(doc))
@@ -45,6 +65,10 @@ export default class Query {
     return this 
   }
 
+  /**
+   * execute the prepared query
+   * can only be run once per query instance
+   */
   exec() {
     if (this.isExecuted) throw new Error ('This query has already been executed')
     
@@ -58,6 +82,10 @@ export default class Query {
   }
 }
 
+/**
+ * helper function to filter a document by a set of tags (using AND)
+ * intended to be run as a filter step in a lodash chain
+ */
 function filterTags (tags = [], doc) {
   if (_.isEmpty(tags)) // no tags provided, filter nothing
     return 'found'
@@ -68,11 +96,21 @@ function filterTags (tags = [], doc) {
   return found
 }
 
+/**
+ * helper function to add # characters before strings
+ */
 function hashTags (tags = []) {
-  // add a # before the tag if it doesn't already exist
   return tags.map(tag => tag.startsWith('#') ? tag : '#' + tag)
 }
 
+/**
+ * helper function to parse a string date range into start and end dates
+ * uses chrono to compute the range defaults to
+ * { 
+ *   start: 6daysago,
+ *   end: today
+ * }
+ */
 function parseDateRange (range) {
   let days = parseInt(range)
   days = days >= 0 ? days : 6 // default 6 days
