d07f3b567eaeb819fda9be8450074d069d39f869
Chad Fawcett <me@chadf.ca>
2016-01-21 14:37:46 -0800

    Bulk delete entries

--- src/commands/tick-rm.js
+++ src/commands/tick-rm.js
@@ -1,8 +1,6 @@
 export default rm
 
-import Entry from '../entry'
-import { getOutputs } from './output'
-import chalk from 'chalk'
+import { removeEntries } from '../remove'
 import db from '../db'
 
 function rm (yargs) {
@@ -13,15 +11,5 @@ function rm (yargs) {
   .argv
 
   argv._.shift()
-  argv._.forEach(id => {
-    db.get(id).then(removeEntry)
-  })
-}
-
-function removeEntry (doc) {
-  const e = Entry.fromJSON(doc)
-  return db.remove(doc).then(result => { 
-    let { detailed } = getOutputs(e)
-    console.log(chalk.bgRed('removed') + ' ' + detailed)
-  })
+  removeEntries(db, argv._)
 }
