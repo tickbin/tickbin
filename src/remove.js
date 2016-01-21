295596aa6f7e319a19f76caf6f078476336f2edf
Chad Fawcett <me@chadf.ca>
2016-01-21 15:13:48 -0800

    Simpler deleting of entries

--- src/remove.js
+++ src/remove.js
@@ -6,24 +6,17 @@ import { getOutputs } from './commands/output'
 export { removeEntries }
 
 function removeEntries (db, ids) {
-  db.allDocs({ keys: ids, include_docs: true })
-  .then(({ rows }) => {
-    //  Fetch docs and mark as deleted
-    var docs = _.chain(rows)
-    .pluck('doc')
-    .forEach(doc => doc._deleted = true)
-    .value()
-
-    //  Bulk delete form db
-    return db.bulkDocs(docs)
-    .then(() => docs)
-  })
-  .then(docs => {
-    docs.forEach(doc => {
+  ids.forEach(id => {
+    db.get(id)
+    .then(doc => {
+      return db.remove(doc)
+      .then(() => doc)  //  Ensure doc is put back on chain
+    })
+    .then(doc => {
       const e = Entry.fromJSON(doc)
       let { detailed } = getOutputs(e)
       console.log(chalk.bgRed('removed') + ' ' + detailed)
     })
+    .catch(err => console.log(err))
   })
-  .catch(err => console.log(err));
 }
