d71bc435effe8389b8fe1bf51eebbffc20a58011
Chris Foster <chris.james.foster@gmail.com>
2016-01-21 16:04:10 -0800

    Add tests

---
+++ src/test/server.test.js
@@ -0,0 +1,60 @@
+import test from 'tape'
+import nock from 'nock'
+import server from '../server'
+
+test('logging into the server', t => {
+
+  t.plan(3)
+
+  let username = 'chrisfosterelli'
+  let password = 'password'
+  let token = '[....]'
+
+  let couch = { 
+    name : username,
+    password : 'a_random_password',
+    url : 'http://[....]'
+  }
+
+  let n = nock('http://localhost:8080/')
+  .post('/token', { username, password })
+  .reply(200, { token })
+  .get('/user')
+  .reply(200, { username, password, couch })
+
+  let p = server.login({ username, password })
+  t.equal(typeof p.then, 'function', 'returns a promise')
+
+  p.then(user => {
+    t.ok(n.isDone(), 'performs request for token and user')
+    t.deepEqual(user, { username, password, couch }, 'returns the user')
+  })
+
+})
+
+test('registering with the server', t => {
+
+  t.plan(3)
+
+  let username = 'chrisfosterelli'
+  let password = 'password'
+
+  let couch = { 
+    name : username,
+    password : 'a_random_password',
+    url : 'http://[....]'
+  }
+
+  let n = nock('http://localhost:8080/')
+  .post('/user', { username, password })
+  .reply(200, { username, password, couch })
+
+  let p = server.register({ username, password })
+  t.equal(typeof p.then, 'function', 'returns a promise')
+
+  p.then(user => {
+    t.ok(n.isDone(), 'performs request for user')
+    t.deepEqual(user, { username, password, couch }, 'returns the user')
+  })
+
+})
