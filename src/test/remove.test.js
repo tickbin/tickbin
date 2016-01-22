2d1c3cd287c7d87c531d7a185383bce3f8fb4a72
Chad Fawcett <me@chadf.ca>
2016-01-22 15:36:04 -0800

    Test that removeEntries returns removed entries

--- src/test/remove.test.js
+++ src/test/remove.test.js
@@ -17,7 +17,7 @@ const rows = [
 var fakeDb = {
   allDocs: function (options) {
     const filteredRows = _.filter(rows, row => {
-      options.keys.indexOf(row.doc._id) > -1
+      return options.keys.indexOf(row.doc._id) > -1
     })
 
     let p = new Promise((resolve, reject) => {
@@ -27,7 +27,7 @@ var fakeDb = {
   },
   bulkDocs: function (docs) {
     let p = new Promise((resolve, reject) => {
-      resolve({ test:'test' })
+      resolve()
     })
     return p
   }
@@ -66,8 +66,8 @@ test('removeEntries requires an array of ids', t => {
 })
 
 test('removeEntries should return a promise', t => {
-  let rowIds = _.chain(rows).pluck('doc').pluck('_id').value()
-  let res    = removeEntries(fakeDb, rowIds)
+  let docIds = _.chain(rows).pluck('doc').pluck('_id').value()
+  let res    = removeEntries(fakeDb, docIds)
 
   t.plan(1)
   t.ok(typeof res.then === 'function', 'removeEntries returns a promise')
@@ -77,10 +77,24 @@ test('removeEntries calls db.allDocs and db.bulkDocs', t => {
   const spyDbAllDocs  = sinon.spy(fakeDb, 'allDocs')
   const spyDbBulkDocs = sinon.spy(fakeDb, 'bulkDocs')
 
-  let rowIds = _.chain(rows).pluck('doc').pluck('_id').value()
-  removeEntries(fakeDb, rowIds).then(() => {
+  let docIds = _.chain(rows).pluck('doc').pluck('_id').value()
+  removeEntries(fakeDb, docIds).then(() => {
     t.plan(2)
     t.ok(spyDbAllDocs.calledOnce, 'called db.allDocs once')
     t.ok(spyDbBulkDocs.calledOnce, 'called db.bulkDocs once')
+  })
+})
+
+test('removeEntries promise resolves to removed docs', t => {
+  let docs   = _.pluck(rows, 'doc')
+  let docIds = _.pluck(docs, '_id')
+
+  removeEntries(fakeDb, docIds).then((removedDocs) => {
+    t.plan(1)
+    t.deepEqual(
+      removedDocs,
+      docs,
+      'removeEntries promise resolves to removed docs'
+    )
   })
 })
