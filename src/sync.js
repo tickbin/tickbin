315905d4ab388ed771d1fd1f66cec5d6242acd85
Jonathan Bowers <jonotron@gmail.com>
2016-01-26 16:05:43 -0800

    test _getLastSync function, pass

--- src/sync.js
+++ src/sync.js
@@ -1,6 +1,23 @@
 export default sync
 
+export { getLastSync as _getLastSync }
+
 function sync(db, dst) {
   if (!db) throw new Error('Please provide a couchdb instance')
   if (!dst) throw new Error('Please provide a destination')
+}
+
+function getLastSync(db) {
+  const lastSync = {
+    _id: '_local/last_sync',
+    push: { last_seq: null },
+    pull: { last_seq: null } 
+  }
+
+  return db.get(lastSync._id)
+  .then(doc => {
+    return doc
+  }, () => {
+    return db.put(lastSync).then(() => db.get(lastSync._id))  
+  })
 }
--- src/test/sync.test.js
+++ src/test/sync.test.js
@@ -3,10 +3,13 @@ import sinon from 'sinon'
 import EventEmitter from 'events'
 
 import sync from '../sync'
+import { _getLastSync } from '../sync'
 
 function getFakeDb() {
   return { 
-    sync: function() {}
+    sync: function() {},
+    get: function() {},
+    put: function() {}
   }
 }
 
@@ -25,6 +28,33 @@ test('sync() requires a db, dst', t => {
   t.plan(2)
   t.throws(syncWithoutDb, /provide a couchdb/, 'sync requires a db')
   t.throws(syncWithoutDst, /provide a destination/, 'sync requires a dst')
+})
+
+test('_getLastSync() gets the last_sync doc', t => {
+  const fakeDb = getFakeDb()
+  const stubGet = sinon.stub(fakeDb, 'get').resolves()
+
+  t.plan(2)
+  _getLastSync(fakeDb).then(() => {
+    t.ok(stubGet.calledOnce, 'get called once')
+    t.ok(stubGet.calledWith('_local/last_sync'), 'gets last_sync') 
+  })
+})
+
+test('_getLastSync() failing to get last_sync, puts last sync', t => {
+  const fakeDb = getFakeDb()
+  const stubGet = sinon.stub(fakeDb, 'get')
+  stubGet.onCall(0).rejects() // simulate last_sync doc not existing
+  stubGet.onCall(1).resolves()
+  const stubPut = sinon.stub(fakeDb, 'put').resolves()
+
+  t.plan(3)
+  _getLastSync(fakeDb).then(() => {
+    const putArg = stubPut.getCall(0).args[0] // the last_sync doc put
+    t.equals(putArg._id, '_local/last_sync', 'puts a new last_sync')
+    t.equals(putArg.push.last_seq, null, 'sets push last_seq null')
+    t.equals(putArg.pull.last_seq, null, 'sets pull last_seq null')
+  })
 })
 
 
