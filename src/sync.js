be31831d50ccf5af2fbbd2b1294201237521d354
Jonathan Bowers <jonotron@gmail.com>
2016-01-29 16:29:39 -0800

    sync is now a simple function that retuns an event emitter, tests pass

--- src/sync.js
+++ src/sync.js
@@ -1,52 +1,36 @@
+import EventEmitter from 'events'
+import forward from 'forward-events'
+
 export default sync
 
 function sync (db, remote) {
   if (!db) throw new Error('Please provide a couchdb instance')
   if (!remote) throw new Error('Please provide a remote destination')
 
-}
-
-class TickSyncer {
-  constructor (db, remote) {
-    this.db = db
-    this.remote = remote
-    this._promiseLastSync = this.getLastSync()
+  const lastSync = {
+    _id: '_local/last_sync',
+    push: { last_seq: null },
+    pull: { last_seq: null } 
   }
-
-  getLastSync() {
-    const lastSync = {
-      _id: '_local/last_sync',
-      push: { last_seq: null },
-      pull: { last_seq: null } 
+  const evt = new EventEmitter()
+  db.get(lastSync._id)
+  .then(doc => doc, () => {
+    return db.put(lastSync).then(() => db.get(lastSync._id)) 
+  })
+  .then(last_sync => {
+    const opts = {
+      push: { since: last_sync.push.last_seq },
+      pull: { since: last_sync.pull.last_seq }
     }
 
-    return this.db.get(lastSync._id)
-    .then(doc => doc, () => {
-      return this.db.put(lastSync).then(() => this.db.get(lastSync._id))  
-    })
-  }
-
-  sync() {
-    return this._promiseLastSync.then(last_sync => {
-      const opts = {
-        push: { since: last_sync.push.last_seq },
-        pull: { since: last_sync.pull.last_seq }
-      }
-      const sync = this.db.sync(this.remote, opts)
-      sync.on('complete', this.updateLastSync.bind(this))
-      // sync is a promise AND an event emitter. Since we are returning
-      // it within a promise, only the resolved value will be
-      // available to anything that calls sync(), not the event emitter itself
-      return sync
-    })
-  }
-
-  updateLastSync(info) {
-    return this._promiseLastSync.then((last_sync) => {
+    const syncEvt = db.sync(remote, opts)
+    forward(syncEvt, evt) // forward all sync events
+    syncEvt.on('complete', info => {
       last_sync.push.last_seq = info.push.last_seq
       last_sync.pull.last_seq = info.pull.last_seq
-      return this.db.put(last_sync)
+      db.put(last_sync)
     })
-  }
+  })
 
+  return evt
 }
--- src/test/sync.test.js
+++ src/test/sync.test.js
@@ -1,5 +1,7 @@
 import test from 'tape'
 import sinon from 'sinon'
+import promised from 'sinon-as-promised'
+import _ from 'lodash'
 import EventEmitter from 'events'
 
 import sync from '../sync'
@@ -12,7 +14,7 @@ function getFakeDb() {
   }
 }
 
-test('sync requires a db, dst', t => {
+test('requires a db, dst', t => {
   function syncWithoutDb() {
     return sync()
   }
@@ -26,67 +28,100 @@ test('sync requires a db, dst', t => {
   t.throws(syncWithoutRemote, /provide a remote/, 'sync requires a remote')
 })
 
-test.skip('new Sync sets db, remote', t => {
+test('return an event emitter', t => {
   const fakeDb = getFakeDb()
-  const stubGet = sinon.stub(fakeDb, 'get').resolves()
-  const tickSync = new TickSyncer(fakeDb, 'remote')
-
-  t.plan(3)
-  t.equals(tickSync.db, fakeDb, 'sets db')
-  t.equals(tickSync.remote, 'remote', 'sets remote')
-  t.ok(stubGet.calledOnce)
-})
-
-test.skip('TickSyncer.getLastSync() gets the last_sync doc', t => {
-  const fakeDb = getFakeDb()
-  const stubGet = sinon.stub(fakeDb, 'get').resolves()
-  const tickSync = new TickSyncer(fakeDb, 'remote')
+  sinon.stub(fakeDb, 'get').resolves()
+  const evt = sync(fakeDb, 'remote')
 
   t.plan(1)
-  tickSync.getLastSync().then(() => {
-    t.ok(stubGet.calledWith('_local/last_sync'), 'gets last_sync') 
-  })
+  t.ok(evt.emit, 'sync returns something with emit()')
 })
 
-test.skip('TickSyncer.sync() calls db.sync', t => {
+test('tries to get last_sync doc', t => {
   const fakeDb = getFakeDb()
-  const last_sync = { push: { last_seq: 10 }, pull: { last_seq: 20 }}
-  const stubGet = sinon.stub(fakeDb, 'get').resolves(last_sync)
-  const evt = new EventEmitter()
-  const stubSync = sinon.stub(fakeDb, 'sync').returns(evt)
+  const stubGet = sinon.stub(fakeDb, 'get').resolves()
 
-  const tickSync = new TickSyncer(fakeDb, 'remote')
+  sync(fakeDb, 'remote')
 
-  t.plan(5)
-  tickSync.sync().then((sync) => {
-    const syncCallArgs = stubSync.getCall(0).args
-    t.ok(stubSync.calledOnce, 'db.sync is called')
-    t.ok(syncCallArgs[0], 'remote', 'sync to remote')
-    t.equals(syncCallArgs[1].push.since, 10, 'push since 10')
-    t.equals(syncCallArgs[1].pull.since, 20, 'pull since 20')
-    t.equals(sync, evt, 'returns the db.sync() event emitter')
-  })
+  const getCall = stubGet.getCall(0)
+  t.plan(2)
+  t.ok(stubGet.calledOnce, 'get only called once')
+  t.ok(getCall.calledWith('_local/last_sync'), 'tries to get last_sync')
 })
 
-test.skip('TickSyncer.sync() updates the last_sync doc', t => {
+test('if last_sync not found, put it', t => {
   const fakeDb = getFakeDb()
-  const last_sync = { push: { last_seq: 10 }, pull: { last_seq: 20 }}
-  const stubGet = sinon.stub(fakeDb, 'get').resolves(last_sync)
+  const stubGet = sinon.stub(fakeDb, 'get')
   const stubPut = sinon.stub(fakeDb, 'put').resolves()
-  const evt = new EventEmitter()
-  const stubSync = sinon.stub(fakeDb, 'sync').returns(evt)
+  stubGet.onCall(0).rejects() // last_sync doesn't exist
+  stubGet.onCall(1).resolves()
 
-  const tickSync = new TickSyncer(fakeDb, 'remote')
-  const complete = { push: { last_seq: 11 }, pull: { last_seq: 21 }}
+  sync(fakeDb, 'remote')
 
   t.plan(3)
-  tickSync.sync().then((sync) => {
-    evt.emit('complete', complete)
-    setTimeout(() => {
-      const putArgs = stubPut.getCall(0).args
-      t.ok(stubPut.calledOnce, 'put called')
-      t.equals(putArgs[0].push.last_seq, 11, 'updates push last_seq')
-      t.equals(putArgs[0].pull.last_seq, 21, 'updates pull last_seq')
-    }, 1)
+  _.defer(() => {
+    t.ok(stubGet.calledTwice, 'calls get twice')
+    t.equals(stubPut.getCall(0).args[0]._id, '_local/last_sync', 'puts last_sync')
+    t.equals(stubGet.getCall(1).args[0], '_local/last_sync', 'gets again')
   })
 })
+
+test('syncs with last_sync info', t => {
+  const last_sync = { push: { last_seq: 10 }, pull: { last_seq: 20 }}
+  const fakeDb = getFakeDb()
+  const stubGet = sinon.stub(fakeDb, 'get').resolves(last_sync)
+  const stubSync = sinon.stub(fakeDb, 'sync')
+
+  sync(fakeDb, 'remote')
+
+  t.plan(4)
+  _.defer(() => {
+    const syncArgs = stubSync.getCall(0).args
+    t.ok(stubSync.calledOnce, 'db.sync called once') 
+    t.equals(syncArgs[0], 'remote', 'syncs with remote')
+    t.equals(syncArgs[1].push.since, 10, 'since last push')
+    t.equals(syncArgs[1].pull.since, 20, 'since last pull')
+  })
+})
+
+test('updates last_sync doc when complete', t => {
+  const last_sync = { _id: '_local/last_sync', push: { last_seq: 10 }, pull: { last_seq: 20 }}
+  const fakeDb = getFakeDb()
+  const stubGet = sinon.stub(fakeDb, 'get').resolves(last_sync)
+  const stubPut = sinon.stub(fakeDb, 'put')
+  const emitter = new EventEmitter()
+  const stubSync = sinon.stub(fakeDb, 'sync').returns(emitter)
+
+  sync(fakeDb, 'remote')
+  t.plan(4)
+  _.defer(() => {
+    const complete = { push: { last_seq: 11 }, pull: { last_seq: 21 }}
+    emitter.emit('complete', complete)
+    const putArgs = stubPut.getCall(0).args
+    t.ok(stubPut.calledOnce, 'put called once')
+    t.equals(putArgs[0]._id, '_local/last_sync', 'updates last_sync')
+    t.equals(putArgs[0].push.last_seq, 11, 'push last_seq from complete info')
+    t.equals(putArgs[0].pull.last_seq, 21, 'pull last_seq from complete info')
+  })
+})
+
+test('db.sync events emit on sync() returned emitter', t => {
+  const last_sync = { _id: '_local/last_sync', push: { last_seq: 10 }, pull: { last_seq: 20 }}
+  const fakeDb = getFakeDb()
+  const stubGet = sinon.stub(fakeDb, 'get').resolves(last_sync)
+  const stubPut = sinon.stub(fakeDb, 'put')
+  const emitter = new EventEmitter()
+  const stubSync = sinon.stub(fakeDb, 'sync').returns(emitter)
+
+  const s = sync(fakeDb, 'remote')
+
+  t.plan(1)
+  _.defer(() => {
+    const payload = { push: { last_seq: 11}, pull: { last_seq: 21 }}
+    s.on('test', info => {
+      t.equals(info, payload, 'complete events forward')
+    }) 
+    emitter.emit('test', payload)
+  })
+})
+
