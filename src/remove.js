c4a1b57a0af9829fcb7ef0fd2416d9307f8b0397
Chad Fawcett <me@chadf.ca>
2016-03-10 08:44:36 -0800

    Remove semicolons

--- src/remove.js
+++ src/remove.js
@@ -18,5 +18,5 @@ function removeEntries (db, ids) {
     return db.bulkDocs(docs)
     .then(() => docs)
   })
-  .catch(err => console.error(chalk.bgRed('error'), err));
+  .catch(err => console.error(chalk.bgRed('error'), err))
 }
