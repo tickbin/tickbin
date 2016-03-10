4e59dde764f7bd3e124f1dc5bc02a103b6894f09
Chad Fawcett <me@chadf.ca>
2016-03-10 09:37:16 -0800

    Use descructuring because it's nicer looking

--- src/test/helpers/chronoDateManipulation.test.js
+++ src/test/helpers/chronoDateManipulation.test.js
@@ -1,7 +1,7 @@
 
 import test from 'tape'
 import chrono from 'chrono-node'
-import * as chronoDateManipulation from '../../helpers/chronoDateManipulation'
+import { add, subtract } from '../../helpers/chronoDateManipulation'
 
 test('add increment to each unit of time', t => {
   //  Test will compare difference against initial start date of
@@ -9,13 +9,13 @@ test('add increment to each unit of time', t => {
   const result = chrono.parse('12:30:30pm-2pm', new Date('Feb 15, 2016 00:00:00'))
   const firstStart = result[0].start
 
-  chronoDateManipulation.add(1, 'year', firstStart)
-  chronoDateManipulation.add(1, 'month', firstStart)
-  chronoDateManipulation.add(1, 'day', firstStart)
-  chronoDateManipulation.add(1, 'hour', firstStart)
-  chronoDateManipulation.add(15, 'minute', firstStart)
-  chronoDateManipulation.add(15, 'second', firstStart)
-  chronoDateManipulation.add(500, 'millisecond', firstStart)
+  add(1, 'year', firstStart)
+  add(1, 'month', firstStart)
+  add(1, 'day', firstStart)
+  add(1, 'hour', firstStart)
+  add(15, 'minute', firstStart)
+  add(15, 'second', firstStart)
+  add(500, 'millisecond', firstStart)
 
   t.equals(firstStart.get('year'), 2017, 'Year incremented properly')
   t.equals(firstStart.get('month'), 3, 'Month incremented properly')
@@ -34,13 +34,13 @@ test('subtract amount to each unit of time', t => {
   const result = chrono.parse('12:30:30pm-2pm', new Date('Feb 15, 2016 00:00:00'))
   const firstStart = result[0].start
 
-  chronoDateManipulation.subtract(1, 'year', firstStart)
-  chronoDateManipulation.subtract(1, 'month', firstStart)
-  chronoDateManipulation.subtract(1, 'day', firstStart)
-  chronoDateManipulation.subtract(1, 'hour', firstStart)
-  chronoDateManipulation.subtract(15, 'minute', firstStart)
-  chronoDateManipulation.subtract(15, 'second', firstStart)
-  chronoDateManipulation.subtract(500, 'millisecond', firstStart)
+  subtract(1, 'year', firstStart)
+  subtract(1, 'month', firstStart)
+  subtract(1, 'day', firstStart)
+  subtract(1, 'hour', firstStart)
+  subtract(15, 'minute', firstStart)
+  subtract(15, 'second', firstStart)
+  subtract(500, 'millisecond', firstStart)
 
   t.equals(firstStart.get('year'), 2015, 'Year decremented properly')
   t.equals(firstStart.get('month'), 1, 'Month decremented properly')
