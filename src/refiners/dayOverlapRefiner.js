dce10cfe0a621515366abeaaddf1675b8c4c995c
Chad Fawcett <me@chadf.ca>
2016-03-14 10:11:42 -0700

    Update dayOverlapRefiner to take advantage of updated chrono

--- src/refiners/dayOverlapRefiner.js
+++ src/refiners/dayOverlapRefiner.js
@@ -28,7 +28,7 @@ function refine (text, results, opt) {
     //  https://github.com/wanasit/chrono/issues/96
     if (moment(result.ref).isSame(today, 'day')
         && result.start.get('meridiem') === 1
-        && result.end.get('hour') < 12) {
+        && result.end.get('meridiem') === 0) {
       subtract(1, 'day', result.start)
       subtract(1, 'day', result.end)
     }
