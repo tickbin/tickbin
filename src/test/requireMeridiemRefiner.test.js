b86109d3789c74ee2132196113d5877012dba606
Jonathan Bowers <jonotron@gmail.com>
2016-04-06 15:29:40 -0700

    test specifying one meridiem does not throw

--- src/test/requireMeridiemRefiner.test.js
+++ src/test/requireMeridiemRefiner.test.js
@@ -7,3 +7,9 @@ test('4-5 throws error', t => {
   t.end()
 })
 
+test('specifying meridiem does not throw', t => {
+  t.doesNotThrow(() => parser('4pm-5 stuff'), 'start has a meridiem')
+  t.doesNotThrow(() => parser('4-5pm stuff'), 'end has a meridiem')
+  t.doesNotThrow(() => parser('4pm-5pm stuff'), 'both has a meridiem')
+  t.end()
+})
