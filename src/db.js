70f00345a55d005c7ba3a42100712370e4fc3f1d
Jonathan Bowers <jonotron@gmail.com>
2016-05-10 14:45:17 -0700

    include pouch-find

--- src/db.js
+++ src/db.js
@@ -3,7 +3,9 @@ import PouchDB from 'pouchdb'
 import chalk from 'chalk'
 import conf from './config'
 import createEntryIndex from './db/designEntryIndex'
+import find from 'pouchdb-find'
 
+PouchDB.plugin(find)
 const db = new PouchDB(conf.db)
 export default db
 
