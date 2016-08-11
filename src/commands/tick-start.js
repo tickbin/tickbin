31622e1d93aac8d7006152cc51e98f6f2af08874
Chad Fawcett <me@chadf.ca>
2016-08-11 16:29:41 -0700

    Fix variable name

--- src/commands/tick-start.js
+++ src/commands/tick-start.js
@@ -2,7 +2,7 @@ import db from '../db'
 
 export default { builder, handler : start}
 
-const defTimerDoc = {
+const defTimersDoc = {
   _id: '_local/timers',
   timers: []
 }
@@ -14,15 +14,15 @@ function builder(yargs) {
 
 function start(argv) {
   //  If the timers document exists, pass it to saveTimer. Otherwise pass the
-  //  defTimerDoc
-  db.get(defTimerDoc._id)
-  .then(timerDoc => saveTimer(timerDoc))
-  .catch(() => saveTimer(defTimerDoc))
+  //  defTimersDoc
+  db.get(defTimersDoc._id)
+  .then(timersDoc => saveTimer(timersDoc))
+  .catch(() => saveTimer(defTimersDoc))
 }
 
-function saveTimer(timerDoc) {
+function saveTimer(timersDoc) {
   //  For now only allow one timer at a time
-  if (timerDoc.timers.length > 0) {
+  if (timersDoc.timers.length > 0) {
     return console.log('You already have a timer running. You can run:\n'
       + '  tick stop: to finish timer and commit entry\n'
       + '  tick cancel-timer: to abort the timer')
@@ -30,9 +30,9 @@ function saveTimer(timerDoc) {
 
   const start = new Date()
 
-  timerDoc.timers.push(start)
+  timersDoc.timers.push(start)
 
-  db.put(timerDoc)
+  db.put(timersDoc)
   .then(() => console.log(`Started timer at ${start}`))
   .catch(err => console.log(`Could not start your timer\n${err.message}`))
 }
