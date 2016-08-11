15877f993a93c5856d7b153f30803b7269310b52
Chad Fawcett <me@chadf.ca>
2016-08-11 16:16:29 -0700

    Basic timer start functionality

--- src/commands/tick-start.js
+++ src/commands/tick-start.js
@@ -1,4 +1,11 @@
+import db from '../db'
+
 export default { builder, handler : start}
+
+const defTimerDoc = {
+  _id: '_local/timers',
+  timers: []
+}
 
 function builder(yargs) {
   return yargs
@@ -6,5 +13,26 @@ function builder(yargs) {
 }
 
 function start(argv) {
-  console.log('Starting a timer')
+  //  If the timers document exists, pass it to saveTimer. Otherwise pass the
+  //  defTimerDoc
+  db.get(defTimerDoc._id)
+  .then(timerDoc => saveTimer(timerDoc))
+  .catch(() => saveTimer(defTimerDoc))
+}
+
+function saveTimer(timerDoc) {
+  //  For now only allow one timer at a time
+  if (timerDoc.timers.length > 0) {
+    return console.log('You already have a timer running. You can run:\n'
+      + '  tick stop: to finish timer and commit entry\n'
+      + '  tick cancel-timer: to abort the timer')
+  }
+
+  const start = new Date()
+
+  timerDoc.timers.push(start)
+
+  db.put(timerDoc)
+  .then(() => console.log(`Started timer at ${start}`))
+  .catch(err => console.log(`Could not start your timer\n${err.message}`))
 }
