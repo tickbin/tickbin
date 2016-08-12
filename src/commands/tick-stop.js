8ee45a46a91d355c568c32db1709770a212b4f7a
Chad Fawcett <me@chadf.ca>
2016-08-11 18:37:23 -0700

    Check for no timers on doc

--- src/commands/tick-stop.js
+++ src/commands/tick-stop.js
@@ -16,6 +16,8 @@ function stop(argv) {
 function stopTimer(timersDoc) {
   const timer = timersDoc.timers.pop()
 
+  if (!timer) return console.log('You do not have a timer started')
+
   db.put(timersDoc)
   .then(() => console.log(`Stopped ${timer} timer`))
   .catch(err => console.log(`Could not stop your timer\n${err.message}`))
