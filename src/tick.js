ccc55d3aed275e1f17e8ef6998e320d8f9cf2bf8
Chad Fawcett <me@chadf.ca>
2016-08-12 15:30:50 -0700

    Add usage example for tick stop

--- src/tick.js
+++ src/tick.js
@@ -22,7 +22,7 @@ yargs
 .command('upgrade', 'upgrade your tickbin', upgrade)
 .command('sync', 'sync your database with remotes', sync)
 .command('start', 'start a timer', start)
-.command('stop', 'stop a timer', stop)
+.command('stop', 'stop a timer and commit entry', stop)
 .version('version', 'prints the current version of tickbin', manifest.version)
 .demand(1)
 .strict()
