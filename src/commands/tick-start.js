b1b00236d00eabe56f1213347f1d2f038fd1fc9e
Chad Fawcett <me@chadf.ca>
2016-08-12 09:49:35 -0700

    Basic start and stop command logic (#263)
    * Basic timer start functionality
    
    * Fix variable name
    
    * Remove timer on tick stop
    
    * Check for no timers on doc
    
    * Basic stop logic
    
    * Make it look pretty! Because Promises.
    
    * Get commit message from user

--- src/commands/tick-start.js
+++ src/commands/tick-start.js
@@ -1,4 +1,11 @@
+import db from '../db'
+
 export default { builder, handler : start}
+
+const defTimersDoc = {
+  _id: '_local/timers',
+  timers: []
+}
 
 function builder(yargs) {
   return yargs
@@ -6,5 +13,26 @@ function builder(yargs) {
 }
 
 function start(argv) {
-  console.log('Starting a timer')
+  //  If the timers document exists, pass it to saveTimer. Otherwise pass the
+  //  defTimersDoc
+  db.get(defTimersDoc._id)
+  .then(timersDoc => saveTimer(timersDoc))
+  .catch(() => saveTimer(defTimersDoc))
+}
+
+function saveTimer(timersDoc) {
+  //  For now only allow one timer at a time
+  if (timersDoc.timers.length > 0) {
+    return console.log('You already have a timer running. You can run:\n'
+      + '  tick stop: to finish timer and commit entry\n'
+      + '  tick cancel-timer: to abort the timer')
+  }
+
+  const start = new Date()
+
+  timersDoc.timers.push(start)
+
+  db.put(timersDoc)
+  .then(() => console.log(`Started timer at ${start}`))
+  .catch(err => console.log(`Could not start your timer\n${err.message}`))
 }
--- src/commands/tick-stop.js
+++ src/commands/tick-stop.js
@@ -1,3 +1,8 @@
+import moment from 'moment'
+import db from '../db'
+import createEntry from '../create'
+import { writeSaved } from './output'
+
 export default { builder, handler : stop}
 
 function builder(yargs) {
@@ -6,5 +11,26 @@ function builder(yargs) {
 }
 
 function stop(argv) {
-  console.log('Stopping a timer')
+  db.get('_local/timers')
+  .then(stopTimer)
+  .then(t => commitTimer(t, argv._[1]))
+  .then(writeSaved)
+  .catch(err => console.log(`Could not stop your timer\n${err.message}`))
+}
+
+function stopTimer(timersDoc) {
+  const timer = timersDoc.timers.pop()
+
+  if (!timer) throw { message: 'You do not have a timer started' }
+
+  return db.put(timersDoc)
+  .then(() => timer)
+}
+
+function commitTimer(timer, message) {
+  const dateFormat = 'MMM D h:mma'
+  const start = moment(timer).format(dateFormat)
+  const stop = moment().format(dateFormat)
+
+  return createEntry(db, `${start} - ${stop} ${message}`)
 }
