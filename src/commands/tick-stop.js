e9b8ec20dfa5913885638307bc9ddfdb1e003bfd
Chad Fawcett <me@chadf.ca>
2016-08-11 18:26:36 -0700

    Remove timer on tick stop

--- src/commands/tick-stop.js
+++ src/commands/tick-stop.js
@@ -1,3 +1,5 @@
+import db from '../db'
+
 export default { builder, handler : stop}
 
 function builder(yargs) {
@@ -6,5 +8,15 @@ function builder(yargs) {
 }
 
 function stop(argv) {
-  console.log('Stopping a timer')
+  db.get('_local/timers')
+  .then(timersDoc => stopTimer(timersDoc))
+  .catch(() => console.log('You do not have a timer started'))
+}
+
+function stopTimer(timersDoc) {
+  const timer = timersDoc.timers.pop()
+
+  db.put(timersDoc)
+  .then(() => console.log(`Stopped ${timer} timer`))
+  .catch(err => console.log(`Could not stop your timer\n${err.message}`))
 }
