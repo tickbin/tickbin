1e6684c849830959fd9317eca61edda05a624caf
Chad Fawcett <me@chadf.ca>
2016-08-18 14:17:06 -0700

    List timer details (#269)

---
+++ src/commands/tick-timer.js
@@ -0,0 +1,24 @@
+import db from '../db'
+import { writeTimer } from './output'
+
+export default { builder, handler: timer }
+
+function builder(yargs) {
+  return yargs
+  .usage('Usage: tick timer')
+}
+
+function timer() {
+  db.get('_local/timers')
+  .then(getTimer)
+  .then(writeTimer)
+  .catch(err => console.log(`Could not list your timer\n${err.message}`))
+}
+
+function getTimer(timersDoc) {
+  const timer = timersDoc.timers[0]
+
+  if (!timer) throw { message: 'You do not have a timer started' }
+
+  return timer
+}
