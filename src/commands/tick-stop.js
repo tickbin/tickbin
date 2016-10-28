2a61d7800b9b735dd23fc6b2f711e2c0c3264370
Chad Fawcett <me@chadf.ca>
2016-10-28 14:31:50 -0700

    Refactor tick-stop command

--- src/commands/tick-stop.js
+++ src/commands/tick-stop.js
@@ -17,53 +17,63 @@ function builder(yargs) {
 function stop(argv) {
   const message = argv._[1]
 
+  let timersDoc
+
   db.get('_local/timers')
-  .then(timersDoc => parseMessage(timersDoc, message))
+  .then(td => {
+    timersDoc = td
+    return timersDoc.timers.pop()
+  })
+  .then(timer => ensureEndAndMessage(timer, message))
+  .then(timer => db.put(timersDoc).then(() => timer))
   .then(timer => commitTimer(db, timer))
   .then(writeSaved)
   .catch(err => console.error(`Could not stop your timer\n${err.message}`))
 }
 
-function parseMessage(timersDoc, newMessage) {
-  const timer = timersDoc.timers.pop()
-
-  if (!timer) throw new Error('You do not have a timer started')
-
-  if (!timer.message && !newMessage) {
-    prompt.message = ''
-    prompt.delimiter = ''
-    prompt.start()
-    return new Promise((resolve, reject) => {
-      prompt.get('message', (err, res) => {
-        if (err && err.message === 'canceled')
-          return reject(new Error('You canceled the stop command'))
-
-        if (err)
-          return reject(err)
-
-        //  When parsing a single date it is always returned as 'start'. Renaming
-        //  to 'end' here for clarity.
-        const { start: end, message } = parser(res.message)
-        timer.end = end || new Date()
-        if (message) timer.message = message
-
-        db.put(timersDoc)
-        .then(() => resolve(timer))
-      })
-    })
+/*
+ * Recursive function to ensure user provides both an end time and message
+ *
+ * This is complicated as the user can input in several different ways and
+ * orders. As well as being able to cancel at any time which should result in
+ * the timer remaining in the couch document.
+ */
+function ensureEndAndMessage(timer, newMessage) {
+  if (!newMessage && !timer.message) {
+    return promptForMessage()
+    .then(message => ensureEndAndMessage(timer, message))
   } else if (newMessage) {
-    //  When parsing a single date it is always returned as 'start'. Renaming
-    //  to 'end' here for clarity.
     const { start: end, message } = parser(newMessage)
-    timer.end = end || new Date()
-    if (message) timer.message = message
 
-    return db.put(timersDoc)
-    .then(() => timer)
+    timer.end = end || timer.end || new Date()
+
+    if (!message && !timer.message) {
+      return promptForMessage()
+      .then(m => ensureEndAndMessage(timer, m))
+    } else {
+      timer.message = message || timer.message
+      return timer
+    }
   } else {
     timer.end = new Date()
 
-    return db.put(timersDoc)
-    .then(() => timer)
+    return timer
   }
+}
+
+function promptForMessage(prevEnd) {
+  return new Promise((resolve, reject) => {
+    prompt.message = ''
+    prompt.delimiter = ''
+    prompt.start()
+    prompt.get('message', (err, res) => {
+      if (err && err.message === 'canceled')
+        return reject(new Error('You canceled the stop command'))
+
+      if (err)
+        return reject(err)
+
+      resolve(res.message)
+    })
+  })
 }
