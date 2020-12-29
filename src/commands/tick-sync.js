6b992727e0f0feb241ac07bf250fb75162d4a203
Caleb Sharp <caleb.sharp@twostoryrobot.com>
2020-12-29 13:52:48 -0800

    Add spinner to sync

--- src/commands/tick-sync.js
+++ src/commands/tick-sync.js
@@ -2,8 +2,11 @@ import db from '../db'
 import conf from '../config'
 import dbsync from '../sync'
 import chalk from 'chalk'
+import ora from 'ora'
 
 export default { builder, handler : sync }
+
+const spinner = ora('Syncing')
 
 function builder(yargs) {
   return yargs
@@ -13,17 +16,29 @@ function builder(yargs) {
 function sync(argv) {
   if (!conf.remote) return console.log('No remote specified in config')
 
+  spinner.start()
   const evt = dbsync(db, conf.remote)
   evt.on('error', handleErr)
   evt.on('denied', handleErr)
   evt.on('complete', showSync)
+  evt.on('change', handleChange)
 }
 
 function handleErr(err) {
   let msg = 'Could not sync with remote CouchDB:'
-  console.error(chalk.bgRed('error'), msg, err.doc.id, err.doc.message, err.doc.reason)
+  spinner.fail(
+    `${chalk.bgRed('error')} ${msg} ${err.doc.id} ${err.doc.message} ${err.doc.reason}`
+  )
 }
 
 function showSync(info) {
-  console.log(`Push: ${info.push.docs_written}  Pull: ${info.pull.docs_written}`)
+  spinner.succeed(`Push: ${info.push.docs_written}  Pull: ${info.pull.docs_written}`)
+}
+
+function handleChange(info) {
+  if (info.change.pending === undefined) return
+  const totalDocs = info.change.pending + info.change.docs_read
+  const elapsed = parseInt(info.change.docs_read)
+  const percentage = Math.round(elapsed / totalDocs * 100)
+  spinner.text = `${percentage}% Synced ${chalk.yellow('›')} ${elapsed}/${totalDocs}`
 }
